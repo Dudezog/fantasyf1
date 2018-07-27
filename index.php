4d119d1fe0cef86657b0ba54e143229a8f98fc76
Nick Herzog <nick@nickherzog.com>
2018-07-27 12:51:26 -0500

    index.php - wrong cookie name was called when checking if set

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 			if(!isset($_REQUEST['content']))
 			{
 				//If cookie set, redirect to members page, else show default page
-				if(isset($_COOKIE['user']))
+				if(isset($_COOKIE['userID']))
 				{
 					echo "<h1>Loading...</h1>";
 					echo "<script> location.replace(\"index.php?content=dash\"); </script>";
